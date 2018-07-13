6407d85bfaba89315ca1a313ab95a484b99cfb9c
MartinRuis <ruismartin98@gmail.com>
2018-07-13 18:56:35 +0200

    Create SomeCode.cs

---
+++ SomeCode.cs
@@ -0,0 +1,13 @@
+Using System;
+
+public class Car (){
+  private string brand;
+  private float speed;
+  private int seats;
+  private int seatsTaken;
+  private int seatsFree;
+  
+  public string Brand { get {return brand;} set {brand = value}};
+  
+  
+}
